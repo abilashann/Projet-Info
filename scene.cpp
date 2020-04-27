b8ac1af299a3f71e689b7b4d40a71552f7eab810
ad123450 <adrien.martinez@edu.ece.fr>
2020-04-27 18:56:02 +0200

    la d

---
+++ scene.cpp
@@ -0,0 +1,23 @@
+#include "scene.h"
+#include "svgfile.h"
+/*
+Scene::Scene()
+{
+
+}
+void Scene::afficherscene() const
+{
+    Svgfile svgout;
+    svgout.addGrid();
+    for (size_t i=0; i<nombre de sommets+1; ++i)
+        {
+        svgout.addCircle(Sommets_x[i],Sommets_y[i],5,"black");
+        svgout.addText(Sommets_x[i],Sommets_y[i]-5,identifiant_Sommets[i],"black");
+        }
+    for (size_t i=0; i<nombre d aretes+1; ++i)
+        {
+        svgout.addLine(Sommets1_x[i],Sommets1_y[i],Sommets2_x[i],Sommets2_y[i],"black");
+        }
+
+}
+*/
---
+++ scene.h
@@ -0,0 +1,10 @@
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+class Scene
+{
+    public:
+        Scene();
+        void afficherscene() const;
+    private:
+};
+#endif // SCENE_H_INCLUDED
