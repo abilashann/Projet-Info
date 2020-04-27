9c413970da6d5ef95373e572a26943e97c40ee8c
ad123450 <adrien.martinez@edu.ece.fr>
2020-04-27 16:29:28 +0200

    C'est une ébauche de l'affichage du graphe
    il me faut la partie d'avant pour pouvoir mettre les bonnes valeurs et changer la classe scene

---
+++ Projet-Info/main.cpp
@@ -0,0 +1,15 @@
+
+#include "svgfile.h"
+#include "scene.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    Scene scene;
+    scene.afficherscene();
+
+    return 0;
+}
